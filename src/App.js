ddf89fb84a0ca3d0ccf52529ef36492eb90d9a33
konstantinreso <88545523+konstantinreso@users.noreply.github.com>
2021-08-23 15:04:11 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import api from "./util/api";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    api.get()
+    .then(({data}) => setTodos(data))
+  }, []);
+
+  function onTodoClick(todo) {
+    api.put(`/${todo.id}`, {...todo, isDone: !todo.isDone})
+    .then(
+      ({data}) =>
+        setTodos(todos.map(item => item.id === todo.id ? data : item))
+    )
+  }
+
+  function saveTodo() {
+    api.post("", {
+      title,
+      isDone: false
+    })
+    .then(({data}) => setTodos([...todos, data]))
+  }
+
+  return (
+    <div className="App">
+      <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <button type="button" onClick={saveTodo}>add</button>
+      <TodoList 
+        items={todos}
+        onItemClick={onTodoClick}
+      />
+    </div>
+  );
+}
+
+export default App;
